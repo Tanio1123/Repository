77d0973bc9788e9550bdf57ce550431233b4ca11
Tanio1123 <54021674+Tanio1123@users.noreply.github.com>
2019-08-12 17:23:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+# Repository
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dragonball</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="home" class="big-bg">
+        <header class="page-header wrapper">
+            <h1><a href="index.php"><img class="logo" src="img/logo_dragonball.gif" alt="dragonballz"></a></h1>
+            <nav class="top-nav">
+                <ul class="main-nav">
+                    <li><a href="index.php">ホーム</a></li>
+                    <li><a href="character.php">キャラクター</a></li>
+                    <li><a href="contact.php">お問い合わせ</a></li>
+                    <li><a href="user.php">ユーザー登録</a></li>
+                    <li><a href="login.php">ログイン</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="home-content wrapper">
+            <h2 class="page-title">DRAGON BALL Z</h2>
+            <p>悟空がいたから楽しかった</p>
+        </div>
+    </div>
+    <div id="home" class="big-bg">
+        <section id="main" class="wrapper">
+            <section id="about" class="about">
+                <h2>ドラゴンボールとは</h2>
+                <p>伝説の玉を求め今度は宇宙を大冒険！人気を不動のものとした国民的アニメの続編。<br></p>
+                <p>『ドラゴンボール』とは、鳥山明による漫画作品。雑誌「週刊少年ジャンプ」にて、1984年51号から1995年25号まで連載されていた。<br>
+                    世界中に散らばった七つの球をすべて集めると、どんな願いも一つだけ叶えられるという秘宝・ドラゴンボールと、主人公・孫悟空を中心に展開する「冒険」「夢」「バトル」「友情」などを描いた長編漫画。<br></p>
+                <p>本作の物語は「孫悟空少年編」、「ピッコロ大魔王編」、「サイヤ人編」、「フリーザ編」、「人造人間・セル編」、「魔人ブウ編」の6つの章からなる。<br>
+                    当初は主にドラゴンボールを探す冒険を描いていたが、「孫悟空少年編」における第21回天下一武道会の頃を契機に、バトル要素を中心にした作品となっていった。<br></p>
+
+                <p>書籍として、単行本が全42巻、完全版が全34巻発売された<br>
+                    単行本の累計発行部数は、完全版を含み国内で1億6000万部以上、全世界累計で2億5000万部以上を記録している。<br></p>
+
+                <p>東映動画（現・東映アニメーション）によってアニメ化され、フジテレビ系列にて放送された。<br>
+                    原作における「孫悟空少年編」から「ピッコロ大魔王編」の部分は『ドラゴンボール』、「サイヤ人編」から最終回は『ドラゴンボールZ』と呼称されており、前者が全153話、後者が全291話製作され、通算444話となっており、両作品は11年にわたって放送され、平均視聴率は20%超を記録した。<br></p>
+                <p> ゲームソフトも多数発売されており、関連ゲームの本数は2017年1月時点で86タイトルにのぼる。<br>
+                    また、2015年までにミリオンセラーを10本以上輩出し、シリーズ累計販売本数は全世界4500万本に達している。<br>
+                    2013年には、「世界で最もビデオゲーム化されたコミック」としてギネス世界記録に認定された。<br></p>
+
+                <p> 連載が終了した後も新作アニメやゲームなどが展開しており、世界中で絶大な人気を誇る、日本の漫画・アニメを代表する作品である。</p>
+            </section>
+        </section>
+    </div>
+    <footer id="footer">
+        Copyright<a href="">Dragonball.</a>All Rights Reserved.
+    </footer>
+
+</body>
+
+</html>
