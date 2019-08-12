a2a1e524bf47785dbaa6227da3dede236d69c832
Tanio1123 <54021674+Tanio1123@users.noreply.github.com>
2019-08-12 17:25:56 +0800

    Create character.php

---
+++ character.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dragonball</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif+JP&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div id="home-character" class="big-bg">
+        <header class="page-header wrapper">
+            <h1><a href="index.php"><img class="logo" src="img/logo_dragonball.gif" alt="dragonballz"></a></h1>
+            <nav class="top-nav">
+                <ul class="main-nav">
+                    <li><a href="index.php">ホーム</a></li>
+                    <li><a href="character.php">キャラクター</a></li>
+                    <li><a href="contact.php">お問い合わせ</a></li>
+                    <li><a href="user.php">ユーザー登録</a></li>
+                    <li><a href="login.php">ログイン</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section id="character">
+            <h2>キャラクター</h2>
+            <section class="character wrapper">
+                <section class="hero">
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E5%AD%AB%E6%82%9F%E7%A9%BA%28%E3%83%89%E3%83%A9%E3%82%B4%E3%83%B3%E3%83%9C%E3%83%BC%E3%83%AB%29" target="_blank"><img class="chara-img" src="img/gpku_super.png" alt="悟空">
+                            <p>孫悟空</p>
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E5%AD%AB%E6%82%9F%E9%A3%AF" target="_blank"><img class="chara-img" src="img/gohan.png" alt="悟飯">
+                            <p>孫悟飯</p>
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E3%83%99%E3%82%B8%E3%83%BC%E3%82%BF" target="_blank"><img class="chara-img" src="img/vegita_super.png" alt="ベジータ">
+                            <p>ベジータ</p>
+                        </a>
+                    </div>
+
+                </section>
+                <section class="villan">
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E3%83%95%E3%83%AA%E3%83%BC%E3%82%B6" target="_blank"><img class="chara-img" src="img/fuliza.png" alt="フリーザ">
+                            <p>フリーザ</p>
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E3%82%BB%E3%83%AB" target="_blank"><img class="chara-img" src="img/seru.jpg" alt="セル">
+                            <p>セル</p>
+                        </a>
+                    </div>
+                    <div class="item">
+                        <a href="https://dic.pixiv.net/a/%E9%AD%94%E4%BA%BA%E3%83%96%E3%82%A6" target="_blank"><img class="chara-img" src="img/buu_fat.png" alt="ブウ">
+                            <p>魔人ブウ</p>
+                        </a>
+                    </div>
+                </section>
+
+            </section>
+        </section>
+    </div>
+    <footer id="footer">
+        Copyright<a href="">Dragonball.</a>All Rights Reserved.
+    </footer>
+</body>
+
+</html>
