7e2d6d66edded9f5e66d30127bd69141fd80a520
tanio1123 <54021674+tanio1123@users.noreply.github.com>
2019-09-12 01:41:24 +0800

    Update user.php

--- user.php
+++ user.php
@@ -60,12 +60,8 @@ if(!empty($_POST)){
                 $user = 'root';
                 $password = 'root';
                 $options = array(
-
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    // デフォルトフェッチモードを連想配列形式に設定
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
-                    // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
                     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
                 );
 
